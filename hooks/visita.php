ad45011e292f08560828edde34bb230746abb8e1
jacaraba <jacaraba@hotmail.com>
2023-02-16 22:16:04 -0500

    inicio

--- hooks/visita.php
+++ hooks/visita.php
@@ -15,10 +15,7 @@
 				break;
 
 			case 'detailview':
-				$header='<script>
-				document.write("The value from the app is<br />" + window.AppInventor.getWebViewString());
-				window.AppInventor.setWebViewString("hello from Javascript")
-			  </script>';
+				$header='';
 				break;
 
 			case 'tableview+detailview':
