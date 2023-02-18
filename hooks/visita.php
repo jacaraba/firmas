02155eb55ab0735dee55829d177650d1388d4f68
jacaraba <jacaraba@hotmail.com>
2023-02-18 13:19:54 -0500

    inicio

--- hooks/visita.php
+++ hooks/visita.php
@@ -104,7 +104,7 @@
 		ob_start(); ?>
 		
 		<script>
-			const l = trim(window.AppInventor.getWebViewString());
+			const l = window.AppInventor.getWebViewString();
 						
 			$j('#latitud').val(l[0]);
 			
